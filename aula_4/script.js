961dd8d439e1103a078e98f7f043abbd9743cbae
unknown <betim009@gmail.com>
2025-01-23 20:37:32 -0300

    update

---
+++ aula_4/script.js
@@ -0,0 +1,13 @@
+/*
+strings, numbers, booleans - array, object
+*/
+
+const target1 = 10
+const target2 = 6
+
+function media(n1, n2) {
+    return n1 + n2 / 2
+}
+
+const media1 = media(target1, target2)
+const media2 = media(10, 9)
