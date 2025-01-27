e8f1534ee2f2b4edad2ec1f6bf13eec5e9f7ed79
unknown <betim009@gmail.com>
2025-01-27 11:51:41 -0300

    update

--- aula_1/script.js
+++ aula_1/script.js
@@ -1,45 +1,25 @@
 import { movies } from "./data.js";
 
 const divTodosFilmes = document.getElementById('todos-filmes');
-const divFilmesFav = document.getElementById('filmes-favoritos');
+const divFilmesFavoritos = document.getElementById('filmes-favoritos');
 
-const filmesFavoritos = [];
-
-movies.map((element) =>
+movies.forEach((element, index) => {
     divTodosFilmes.innerHTML += `
-        <div>
-            <h2>${element.title}</h2>
-            <img src="${element.poster}" />
-
-            <div class="div-favoritar">
-
-            </div>
+    <div> 
+        <img src="${element.poster}" />
+        <h2>${element.title}</h2>
+        <div class="footer-filme"> 
+        
         </div>
+    </div>
     `
-);
-const divsFavoritar = document.querySelectorAll('.div-favoritar');
-
-divsFavoritar.forEach((element, index) => {
-    const btnFavoritar = document.createElement('button');
-    btnFavoritar.textContent = "Favoritar";
-    element.appendChild(btnFavoritar);
-
-    btnFavoritar.addEventListener('click', () => {
-        alert();
-        filmesFavoritos.push(movies[index]);
-        console.log(filmesFavoritos);
-
-        filmesFavoritos.map((element) =>
-            divFilmesFav.innerHTML += `
-                <div>
-                    <h2>${element.title}</h2>
-                    <img src="${element.poster}" />
-        
-                    <div class="div-favoritar">
-        
-                    </div>
-                </div>
-            `
-        );
-    });
 });
+const footerFilmes = document.querySelectorAll('.footer-filme');
+
+footerFilmes.forEach((element, index) => {
+    element.innerHTML += `
+        <button> Favoritar </button>
+    `
+});
+
+console.log(footerFilmes);
